d7ba8aaa6a8cedc327d344f1d21b2950fdcebe70
busayolawumi <busayoolawumi3@gmail.com>
2024-04-18 00:22:32 +0100

    fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,29 +1,10 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    next() {
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-
-      const currentDepartment = departments[currentDepartmentIndex];
-      const currentEmployee = currentDepartment[currentEmployeeIndex];
-
-      currentEmployeeIndex++;
-
-      if (currentEmployeeIndex >= currentDepartment.length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-      }
-
-      return { value: currentEmployee, done: false };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
 }
