d93cb346212dfcdc2b6f9855662ef66e391a2e42
busayolawumi <busayoolawumi3@gmail.com>
2024-04-18 00:19:54 +0100

    fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-for (let value of array) {
-value = appendString + value;
-}
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
+  }
 
-return array;
+  return arr;
 }
