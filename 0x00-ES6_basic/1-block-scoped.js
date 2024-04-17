b48e72dceaa30a14984add9204f6630ed22d50e0
busayolawumi <busayoolawumi3@gmail.com>
2024-04-18 00:10:24 +0100

    fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-   
+    return [task, task2];
   }
-
   return [task, task2];
 }
