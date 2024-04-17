e3abddc41e5d62aa871285043f805a05406b1b4e
busayolawumi <busayoolawumi3@gmail.com>
2024-04-18 00:21:03 +0100

    fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,14 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-  
-  for (const departmentName in employeesList) {
-    allEmployees[departmentName] = [...employeesList[departmentName]];
-  }
-
   return {
-    allEmployees: allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    }
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
   };
 }
